b49b36177f3575154703b6a6f9ef56638bb11f8d
Yegor Trussov <egortr2004@mail.ru>
2020-08-23 10:37:04 +0600

    template inheritance, sessions, request types

--- tutorial1/script.py
+++ tutorial1/script.py
@@ -1,12 +1,37 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
+
+from datetime import timedelta
 
 app = Flask(__name__) 
+app.secret_key = 'keyboardcat'
+app.permanent_session_lifetime = timedelta(seconds=10)
 
-@app.route('/<name>')
-def home(name):
-    return render_template('index.html', name=name, names=['12', '15', '55'])
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if (request.method == 'POST'):
+        session.permanent = False
+        usr = request.form['name']
+        session['user'] = usr
+        return redirect(url_for('user'))
+    else:
+        return render_template('login.html')
 
+@app.route('/user')
+def user():
+    if 'user' in session:
+        user = session['user']
+        return render_template('profile.html', user=user)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
